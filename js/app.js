00b10291ef48f23c274bec40d11ab89c6f8719dd
Jeremiah Lewis <jclewis1914@gmail.com>
2025-03-16 02:30:09 -0600

    Footer Hides on Start
    Footer gets the class .hidden to it when the game starts

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@
 const startButton = document.querySelector('.btn_reset');
 const overlay = document.querySelector('#overlay');
 const gameContainer = document.querySelector('.main-container');
+const footer = document.querySelector('#pageFooter');
 let wrongGuessCounter = 0;
 
 //Start Button on Overlay
 startButton.addEventListener('click', () => {
     overlay.classList.add('hidden');
+    footer.classList.add('hidden');
     gameContainer.style.display = 'block';
 });
