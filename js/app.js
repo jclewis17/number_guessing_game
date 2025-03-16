01e27af6f0628515d53bba134937c62ad434a5a2
Jeremiah Lewis <jclewis1914@gmail.com>
2025-03-16 04:16:17 -0600

    Added Rules Overlay
    Added a secondary overlay to explain rules on the start screen

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
 // Variables
 const startButton = document.querySelector('.btn_reset');
+const rulesButton = document.querySelector('.rules');
 const overlay = document.querySelector('#overlay');
+const rulesOverlay = document.querySelector('#rulesOverlay'); 
+const closeRulesButton = document.querySelector('#closeRules'); 
 const gameContainer = document.querySelector('.main-container');
 const footer = document.querySelector('#pageFooter');
 let wrongGuessCounter = 0;
 
-//Start Button on Overlay
+//Start Button on Main Overlay
 startButton.addEventListener('click', () => {
     overlay.classList.add('hidden');
     footer.classList.add('hidden');
     gameContainer.style.display = 'block';
 });
+
+//How to Play Button
+rulesButton.addEventListener('click', () => {
+    rulesOverlay.classList.add('active');
+});
+
+//X Button to close the rules
+closeRulesButton.addEventListener('click', () => {
+    rulesOverlay.classList.remove('active');
+});
+
+
